02576496210db72093d0aa44da9efba4a1d4ceb0
Benjamín Larenas <alonsobenjaminlb@gmail.com>
2022-09-12 15:42:18 -0400

    prueba

--- database/database/database.js
+++ database/database/database.js
@@ -2,9 +2,9 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize('bsale_test', 'bsale_test', 'bsale_test', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD , {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: process.env.DB_DIALECT,
 });
 
 module.exports = sequelize;
